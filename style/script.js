2531de32e4970c285548f2eb946c9f90e96bd3ab
danyel <danyelsilva320@gmail.com>
2019-06-16 14:04:53 -0300

    projeto semi finalizado

--- style/script.js
+++ style/script.js
@@ -1,1 +1,9 @@
-var
+
+function add ( valor ){
+    var visor = document.calculadora.visor.value;
+    document.calculadora.visor.value = visor + valor;
+}
+function resultado(){
+    var visor = document.calculadora.visor.value
+    document.calculadora.visor.value = eval(document.calculadora.visor.value);
+}
